868a5ee710849d791ad365c86beac6bc93ae8195
Devansh Tripathi <devansh_tripathi05@outlook.com>
2023-09-15 15:52:19 +0530

    Student-log-in_page

--- Web-dev/2nd_page.js
+++
@@ -1,7 +0,0 @@
-// Get all elements with class name 'sub'
-var elements = document.getElementsByClassName('sub');
-
-// Loop through the elements and hide them
-for (var i = 0; i < elements.length; i++) {
-  elements[i].style.display = 'none';
-}
