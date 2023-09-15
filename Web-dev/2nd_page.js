edc66c6195ece1aad88bcd24d9350686d4ff9add
Devansh Tripathi <devansh_tripathi05@outlook.com>
2023-09-15 14:19:13 +0530

    front-end_file

---
+++ Web-dev/2nd_page.js
@@ -0,0 +1,7 @@
+// Get all elements with class name 'sub'
+var elements = document.getElementsByClassName('sub');
+
+// Loop through the elements and hide them
+for (var i = 0; i < elements.length; i++) {
+  elements[i].style.display = 'none';
+}
