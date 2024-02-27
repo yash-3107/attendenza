d9f5897d34ea8ed99e9069165d1c7534fdbf4193
Devansh Tripathi <devansh_tripathi05@outlook.com>
2024-02-27 18:42:41 +0530

    add all relevant-files

---
+++ Web-dev/Admin_login/chart1.js
@@ -0,0 +1,21 @@
+var ctx = document.getElementById('lineChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets: [{
+            label: 'student attendance ',
+            data: [1401, 1320, 1185, 1500, 980, 1152, 1489, 1515, 1482, 1252, 1285, 1502],
+            backgroundColor: [
+                'rgba(85,85,85, 1)'
+
+            ],
+            borderColor: 'rgb(41, 155, 99)',
+
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
---
+++ Web-dev/login_interface_stu/README.md
@@ -0,0 +1,3 @@
+# student-dashboard-for-attendance
+this is simple dashboard for studnet-login interface 
+using Html5 and CSS3
